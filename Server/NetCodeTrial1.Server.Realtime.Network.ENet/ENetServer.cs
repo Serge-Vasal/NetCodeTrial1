624f7ed6bac6fe320027ffde14a527fdf126bbe3
Sergey Vasalaty <sergey.vasalaty@gmail.com>
2020-08-06 13:05:27 +0300

    Simplify ENetServer

--- Server/NetCodeTrial1.Server.Realtime.Network.ENet/ENetServer.cs
+++ Server/NetCodeTrial1.Server.Realtime.Network.ENet/ENetServer.cs
@@ -1,4 +1,5 @@
 ﻿using ENet;
+using Microsoft.Extensions.Logging;
 using NetCodeTrial1.Server.Realtime.Contracts;
 using System;
 using System.Threading;
@@ -8,27 +9,27 @@ namespace NetCodeTrial1.Server.Realtime.Network.ENet
 {
     public class ENetServer : INetworkServer
     {
-        private Host server;
-        private Address address;
+        private readonly ILogger<ENetServer> logger;
 
+        private Host server;
         private CancellationTokenSource cancellationTokenSource;
 
-        public ENetServer()
+        public ENetServer(ILogger<ENetServer> logger)
         {
+            this.logger = logger;
         }
 
         public Task Start(CancellationToken cancellationToken)
         {
             cancellationTokenSource = new CancellationTokenSource();
-
             server = new Host();
-
-            address = default;
+            Address address = default;
             address.Port = 40002;
             server.Create(address, 10, 4);
 
             Task.Factory.StartNew(() => RunLoop(server, cancellationTokenSource.Token), TaskCreationOptions.LongRunning);
 
+            logger.LogInformation("Enet server started.");
             return Task.CompletedTask;
         }
 
@@ -38,50 +39,46 @@ namespace NetCodeTrial1.Server.Realtime.Network.ENet
             {
                 while (!cancellationToken.IsCancellationRequested)
                 {
-                    Event netEvent;
+                    server.Service(15, out Event netEvent);
 
-                    while (!Console.KeyAvailable)
-                    {
-                        bool polled = false;
-
-                        while (!polled)
-                        {
-                            if (server.CheckEvents(out netEvent) <= 0)
-                            {
-                                if (server.Service(15, out netEvent) <= 0)
-                                    break;
-
-                                polled = true;
-                            }
-
-                            switch (netEvent.Type)
-                            {
-                                case EventType.None:
-                                    break;
-
-                                case EventType.Connect:
-                                    Console.WriteLine("Connect Event received from client - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                                    break;
-
-                                case EventType.Disconnect:
-                                    Console.WriteLine("Disconnect Event received from client - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                                    break;
-
-                                case EventType.Timeout:
-                                    Console.WriteLine("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                                    break;
-
-                                case EventType.Receive:
-                                    Console.WriteLine("Packet received from - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP + ", Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
-                                    netEvent.Packet.Dispose();
-                                    break;
-                            }
-                        }
-                    }
+                    Process(ref netEvent);
                 }
             }
             catch (Exception e)
             {
+                logger.LogError(e.Message);
+            }
+        }
+
+        private void Process(ref Event networkEvent)
+        {
+            switch (networkEvent.Type)
+            {
+                case EventType.None:
+                    break;
+
+                case EventType.Connect:
+                    logger.LogInformation("Client connected - ID: {peerId}, IP: {peerIP}", networkEvent.Peer.ID, networkEvent.Peer.IP);
+                    Console.WriteLine("Connect Event received from client - ID: " + networkEvent.Peer.ID + ", IP: " + networkEvent.Peer.IP);
+                    break;
+
+                case EventType.Disconnect:
+                    logger.LogInformation("Client disconnected - ID: {peerId}, IP: {peerIP}", networkEvent.Peer.ID, networkEvent.Peer.IP);
+                    Console.WriteLine("Disconnect Event received from client - ID: " + networkEvent.Peer.ID + ", IP: " + networkEvent.Peer.IP);
+                    break;
+
+                case EventType.Timeout:
+                    logger.LogInformation("Client timeout - ID: {peerId}, IP: {peerIP}", networkEvent.Peer.ID, networkEvent.Peer.IP);
+                    Console.WriteLine("Client timeout - ID: " + networkEvent.Peer.ID + ", IP: " + networkEvent.Peer.IP);
+
+                    break;
+
+                case EventType.Receive:
+                    Console.WriteLine("Packet received from - ID: " + networkEvent.Peer.ID + ", IP: " + networkEvent.Peer.IP + ", Channel ID: " + networkEvent.ChannelID + ", Data length: " + networkEvent.Packet.Length);
+
+                    break;
+                default:
+                    throw new NotImplementedException($"The event type {networkEvent.Type} is not supported.");
             }
         }
 
